6b34b89bbcdb9d250c888a708317a7dcf2722232
arualnonnag <128794812+arualnonnag@users.noreply.github.com>
2023-06-28 18:47:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+
+function hideunhide(id){
+    const p1 = document.getElementById(id);
+    if(p1.style.display == "none"){
+        p1.style.display ="block";
+    }
+else{
+    p1.style.display = "none"
+}
+ }
+
+
+ let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+ function myFunction() {
+    var x = document.getElementById("myBtn").name;
+    document.getElementById("demo").innerHTML = x;
+  }
